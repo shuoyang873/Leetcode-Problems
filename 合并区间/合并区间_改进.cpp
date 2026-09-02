cf2770c0ab4d41821ee516712bb89faeb2131382
shuoyang873 <826642340@qq.com>
2026-09-02 12:30:34 +0800

    add 搜索二维矩阵_第一版_递归_大数据超时

---
+++ 合并区间/合并区间_改进.cpp
@@ -0,0 +1,80 @@
+/*
+56. 合并区间
+已解答
+中等
+相关标签
+premium lock icon
+相关企业
+以数组 intervals 表示若干个区间的集合，其中单个区间为 intervals[i] = [starti, endi] 。请你合并所有重叠的区间，并返回 一个不重叠的区间数组，该数组需恰好覆盖输入中的所有区间 。
+
+ 
+
+示例 1：
+
+输入：intervals = [[1,3],[2,6],[8,10],[15,18]]
+输出：[[1,6],[8,10],[15,18]]
+解释：区间 [1,3] 和 [2,6] 重叠, 将它们合并为 [1,6].
+示例 2：
+
+输入：intervals = [[1,4],[4,5]]
+输出：[[1,5]]
+解释：区间 [1,4] 和 [4,5] 可被视为重叠区间。
+示例 3：
+
+输入：intervals = [[4,7],[1,4]]
+输出：[[1,7]]
+解释：区间 [1,4] 和 [4,7] 可被视为重叠区间。
+ 
+
+提示：
+
+1 <= intervals.length <= 104
+intervals[i].length == 2
+0 <= starti <= endi <= 104
+*/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {        //这题的思路是参考贪心算法的活动选择这个思路
+        int n=intervals.size();
+        if(n==0||n==1)return intervals;
+        vector<vector<int>>ans;
+        sort(intervals.begin(),intervals.end());                            //这边的答案是直接采用sort函数，代码量比较少，但是相比于我之前的qsort，时间会慢一点
+        int start=intervals[0][0],end=intervals[0][1];
+        for(int i=1;i<n;i++){
+            if(intervals[i][0]<=end){                      //如果当前数据的start<=end，说明这两个区间有重合，可以合并
+                if(end<=intervals[i][1]){
+                    end=intervals[i][1];                     //同时要特殊处理一下，防止end的值大于当前数据的结尾数字，导致更新出错
+                }
+            }
+            else{
+                ans.push_back({start,end});
+                start=intervals[i][0];          //这里要同时更新start和end
+                end=intervals[i][1];
+            }
+        }
+        ans.push_back({start,end});             //最后一组数据还要手动push_back，防止遗漏
+        return ans;
+    }
+};
+
+//如：解答错误
+// 79 / 172 个通过的测试用例
+// 提交于 2026.09.02 11:11
+
+// 分析
+
+// 官方题解
+// 输入
+// intervals =
+// [[1,4],[2,3]]
+
+// 添加到测试用例
+// 输出
+// [[2,4]]
+// 预期结果
+// [[1,4]]
