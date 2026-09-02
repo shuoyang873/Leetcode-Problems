da58816bbff9b7511116a5ab9a70d56d023ec5eb
shuoyang873 <826642340@qq.com>
2026-09-02 16:57:39 +0800

    add 二叉树的最大直径_深度优先搜索

---
+++ 二叉树的最大直径.cpp
@@ -0,0 +1,68 @@
+/*
+
+代码
+测试用例
+测试用例
+测试结果
+543. 二叉树的直径
+已解答
+简单
+相关标签
+premium lock icon
+相关企业
+给你一棵二叉树的根节点，返回该树的 直径 。
+
+二叉树的 直径 是指树中任意两个节点之间最长路径的 长度 。这条路径可能经过也可能不经过根节点 root 。
+
+两节点之间路径的 长度 由它们之间边数表示。
+
+ 
+
+示例 1：
+
+
+输入：root = [1,2,3,4,5]
+输出：3
+解释：3 ，取路径 [4,2,1,3] 或 [5,2,1,3] 的长度。
+示例 2：
+
+输入：root = [1,2]
+输出：1
+ 
+
+提示：
+
+树中节点数目在范围 [1, 104] 内
+-100 <= Node.val <= 100
+*/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+ struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ };
+
+ class Solution {
+public:
+    int ans=1;
+    int depth(TreeNode*root){
+        if(root==nullptr)return 0;
+        int l=depth(root->left);
+        int r=depth(root->right);
+        int len=l+r+1;
+        ans=max(ans,len);
+        return max(l,r)+1;
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+        if(root==nullptr)return 0;
+        depth(root);
+        return ans-1;
+    }
+};
