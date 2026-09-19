10a3b57b0f04d37c5b3327189afed5a941e789d5
shuoyang873 <826642340@qq.com>
2026-09-19 10:29:35 +0800

    add 圆和矩形是否重叠_2.0

---
+++ 力扣/圆和矩形是否有重叠_2.0.cpp
@@ -0,0 +1,53 @@
+/*
+
+代码
+测试用例
+测试用例
+测试结果
+1401. 圆和矩形是否有重叠
+已解答
+中等
+相关标签
+premium lock icon
+相关企业
+提示
+给你一个以 (radius, xCenter, yCenter) 表示的圆和一个与坐标轴平行的矩形 (x1, y1, x2, y2) ，其中 (x1, y1) 是矩形左下角的坐标，而 (x2, y2) 是右上角的坐标。
+
+如果圆和矩形有重叠的部分，请你返回 true ，否则返回 false 。
+
+换句话说，请你检测是否 存在 点 (xi, yi) ，它既在圆上也在矩形上（两者都包括点落在边界上的情况）。
+
+ 
+
+示例 1 ：
+
+
+输入：radius = 1, xCenter = 0, yCenter = 0, x1 = 1, y1 = -1, x2 = 3, y2 = 1
+输出：true
+解释：圆和矩形存在公共点 (1,0) 。
+示例 2 ：
+
+输入：radius = 1, xCenter = 1, yCenter = 1, x1 = 1, y1 = -3, x2 = 2, y2 = -1
+输出：false
+示例 3 ：
+
+
+输入：radius = 1, xCenter = 0, yCenter = 0, x1 = -1, y1 = 0, x2 = 0, y2 = 1
+输出：true
+ 
+*/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    bool checkOverlap(int radius, int xCenter, int yCenter, int x1, int y1, int x2, int y2) {
+        //这边采用clamp这个函数，直接找到圆心到矩形的最近点的水平和竖直距离
+        int dis_x=clamp(xCenter,x1,x2)-xCenter;
+        int dis_y=clamp(yCenter,y1,y2)-yCenter;
+        return radius*radius>=dis_x*dis_x+dis_y*dis_y;     //直接通过距离和r的关系来判断是否与矩形有重叠
+    }
+};
